7f0cf05dfb776b84f18439419c1d1bc6a7dca866
Hugh Potts <hugh.potts@triogen.com>
2025-02-03 19:24:53 +0000

    Functions rationalised, tested

--- bridgeDriverPWM/bridgeDriverPWM.h
+++ bridgeDriverPWM/bridgeDriverPWM.h
@@ -1,45 +1,70 @@
 #ifndef BRIDGE_DRIVER_PWM_H_INCLUDED
 #define BRIDGE_DRIVER_PWM_H_INCLUDED
 
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+enum pwm_error_t {
+    PWM_OK = 0,
+    PWM_FREQ_TOO_HIGH = 11,
+    PWM_FREQ_TOO_LOW = 12,
+    PWM_DUTY_TOO_HIGH = 21,
+    PWM_DUTY_TOO_LOW = 22,
+    PWM_SET_TR_INVALID_COUNTS = 31,
+    PWM_NOT_INITIALISED = 99
+};
+
+// for almist all functions the 'channel' argument is optional, and will default to channel 0 if not specified
 
 class bridgeDriverPWM {
   public:
-    bridgeDriverPWM(uint pin, uint32_t initial_frequency, uint32_t initial_duty, PIO pio=pio0);     //pio0 or pio1
+    bridgeDriverPWM(uint pin, uint32_t initial_frequency, uint32_t initial_duty, PIO pio = pio0); // pio0 or pio1
     bool addChannel(uint pin, uint32_t initial_frequency, uint32_t initial_duty);
-    uint32_t setFrequency(uint channel, uint32_t frequency);
-    uint32_t setFrequency(uint32_t frequency) { return setFrequency(0, frequency);};  // default to ch0
-    
-    uint32_t setDuty(uint channel, uint duty1000);  
 
-    bool setFreqDuty(uint channel, uint32_t frequency, uint32_t duty1000);
+    pwm_error_t setFrequency(uint channel, uint32_t frequency);
+    pwm_error_t setFrequency(uint32_t frequency) { return setFrequency(0, frequency); }; // default to ch0
 
-    void setTimingRegister(uint channel, uint32_t regval);                      // manually set timing registers
-    void setTimingRegister(uint channel, uint16_t on_time, uint16_t off_time) {setTimingRegister(channel, (uint32_t) ((on_time-3)<<16 | (off_time-3)));};   // manually set timing registers
-    
-    uint32_t getReqFrequency(uint channel=0) {return _requested_freq[channel];};  
-    uint32_t getFrequency(uint channel=0) {return _actual_freq[channel];};
+    pwm_error_t setDuty(uint channel, uint duty1000);
+    pwm_error_t setDuty(uint duty1000) { return setDuty(0, duty1000); };
 
-    uint32_t getReqDuty(uint channel=0) {return _requested_freq[channel];};  
-    uint32_t getDuty(uint channel=0) {return _actual_freq[channel];};
+    pwm_error_t setFreqDuty(uint channel, uint32_t frequency, uint32_t duty1000);
+    pwm_error_t setFreqDuty(uint32_t frequency, uint32_t duty1000) { return setFreqDuty(0, frequency, duty1000); };
 
+    void setTimingRegister(uint channel, uint32_t regval);   // manually set timing registers, not normally used
+    void setTimingRegister(uint channel, uint16_t on_time, uint16_t off_time); // as above but explicit on and off counts
+
+    uint32_t getRequestedFrequency(uint channel = 0) { return _requested_freq[channel]; };
+    uint32_t getFrequency(uint channel = 0);
+
+    uint32_t getRequestedDuty(uint channel = 0) { return _requested_duty[channel]; };
+    uint32_t getDuty(uint channel = 0);
+
+    pwm_error_t get_status(uint channel = 0) { return _status[channel]; };
     void enable(uint channel, bool state);
-    void enable(bool state) {enable(0, state);}; 
+    void enable(bool state) { enable(0, state); };
 
     void allOff(void);
 
+
+    const uint32_t max_freq = SYS_CLK_HZ / (2*24);            // max frequency that allows 8 evenly spaced steps of PWM
+    const uint32_t min_freq = SYS_CLK_HZ / (2 * 0xffff);      // min frequency with counter at max val
+
+//           RP2040   RP2350
+// max_freq  2.604    3.125      MHz
+// min_freq  954      1144       Hz      
+// XXXTODO: add in clock divider to extend to lower frequency ranges
+
   private:
-    PIO _pio;         // which PIO we are using pio0 or pio1
-    uint _offset;     // memory offset of the program
+    PIO _pio;     // which PIO we are using pio0 or pio1
+    uint _offset; // memory offset of the program
     uint _pin[4];
-    uint32_t _requested_freq[4] = {0};    // set frequency
-    uint32_t _actual_freq[4]    = {0};    // actual frequency
-    uint32_t _requested_duty[4] = {0};    // set frequency
-    uint32_t _actual_duty[4]    = {0};    // actual frequency
-    uint32_t _regval[4]         = {0};
-    uint _n_channels;   // number of active channels
+    uint _n_channels;              // number of active channels
+    uint32_t _requested_freq[4]; 
+    uint32_t _requested_duty[4];
+    uint32_t _regval[4];
+    pwm_error_t _status[4]; 
+
     uint32_t _calculateTimingRegisters(uint32_t freq, uint32_t duty);
 };
 
--- bridgeDriverPWM/bridgeDriverPWM_example.cpp
+++ bridgeDriverPWM/bridgeDriverPWM_example.cpp
@@ -8,18 +8,17 @@
 
 */
 
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
 #include "bridgeDriverPWM.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 #ifndef OUTPUT_PIO0_CH0
 #define OUTPUT_PIO0_CH0 10
 #define OUTPUT_PIO0_CH1 1 2
 #endif
 
-int main()
-{
+int main() {
     stdio_init_all();
     sleep_ms(1000); // just to give time for Serial to initialise
 
@@ -30,20 +29,48 @@ int main()
     // uint32_t init_vals = 0<<16 | 0;
     uint32_t init_vals = 197 << 16 | 47;
 
-    bridgeDriverPWM bridge_driver0(OUTPUT_PIO0_CH0, 250000, 10); // if pio not specified, use pio0
-    bridge_driver0.setFrequency(0, 250000);
-    bridge_driver0.setDuty(0, 100);
-
+    // simplest usage
+    bridgeDriverPWM bridge_driver0(OUTPUT_PIO0_CH0, 250000, 100); // if pio not specified, use pio0
     bridge_driver0.enable(0, true);
 
-    while (true)
-    {
-        for (int i = 1; i < 992; i++)
-        {
-            bridge_driver0.setDuty(0, i);
-            sleep_ms(5);
+    while (true) {
+        // set maximum frequency, minimum duty - shiould be on for 3 clock cycles
+        bridge_driver0.setFreqDuty(bridge_driver0.max_freq, 1);
+        sleep_ms(1000);
+        // set maximum frequency, maximum duty - shiould be off for 3 clock cycles
+        bridge_driver0.setFreqDuty(bridge_driver0.max_freq, 1000);
+        sleep_ms(1000);
+
+        // ramp min-max duty at high freq, showing coarse steps
+        bridge_driver0.setDuty(0); // working
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 1000; i++) {
+                bridge_driver0.setDuty(0, i);
+                sleep_ms(1);
+            }
         }
-        sleep_ms(2000);
+        // set to 20kHZ and change the frequency at constant 50% duty
+        uint32_t f_start = 20000;
+        uint32_t f_stop = 100000;
+        uint32_t f_step = 50;
+        bridge_driver0.setFreqDuty(f_start, 500);
+        sleep_ms(1000);
+        for (int j = 0; j < 3; j++) {
+            for (uint32_t i = f_start; i < f_stop; i+=f_step) {
+                bridge_driver0.setFrequency(i);
+                sleep_ms(1);
+            }
+        }
+        
+        // change duty and frequency to keep the on time constant, but change the duty by changing the frequency
+        // demonstrates using timing registers manually - cam be useful for driving systems with multiple resonances
+        uint32_t ontime = (SYS_CLK_HZ * 10) / 1e6;  // (10.0us on time)
+        for (int j = 0; j < 3; j++) {
+            for (uint32_t offtime = 10*ontime; offtime >0; offtime--) {
+                bridge_driver0.setTimingRegister(0, ontime, offtime);   // note channel number (0) is needed
+                sleep_ms(1);
+            }
+        }
+
     }
-    // bridge_driver0.setFrequency(init_vals);
 }
